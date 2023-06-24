9b584ea745330e59c51c981bca918a1f22b516e8
zsenarchitect <zsenarchitect@gmail.com>
2023-06-24 16:42:06 -0400

    add comment

--- Monopoly.extension/Monopoly.tab/Game.panel/Game.pushbutton/game_script.py
+++ Monopoly.extension/Monopoly.tab/Game.panel/Game.pushbutton/game_script.py
@@ -15,17 +15,17 @@ from Autodesk.Revit.UI import IExternalEventHandler, ExternalEvent
 
 from Autodesk.Revit.Exceptions import InvalidOperationException
 from pyrevit.forms import WPFWindow
-from pyrevit import forms #
-from pyrevit import script #
+from pyrevit import forms
+from pyrevit import script
 
 
-from Autodesk.Revit import DB # fastest DB
+from Autodesk.Revit import DB  # fastest DB
 from Autodesk.Revit import UI
 try:
     uidoc = __revit__.ActiveUIDocument
     doc = __revit__.ActiveUIDocument.Document
 except:
-    print 
+    print
     "nnned to allow open revit doc when zero doc."
 
 __persistentengine__ = True
@@ -42,8 +42,6 @@ def play(title):
     t.Commit()
 
 
-
-
 # Create a subclass of IExternalEventHandler
 class SimpleEventHandler(IExternalEventHandler):
     """
@@ -57,23 +55,22 @@ class SimpleEventHandler(IExternalEventHandler):
         self.kwargs = None
         self.OUT = None
 
-
     # Execute method run in Revit API environment.
+
     def Execute(self,  uiapp):
         try:
             try:
-                #print "try to do event handler func"
+                # print "try to do event handler func"
                 self.OUT = self.do_this(*self.kwargs)
             except:
-                print ("failed")
-                print (traceback.format_exc())
+                print("failed")
+                print(traceback.format_exc())
         except InvalidOperationException:
             # If you don't catch this exeption Revit may crash.
-            print ("InvalidOperationException catched")
+            print("InvalidOperationException catched")
 
     def GetName(self):
         return "simple function executed by an IExternalEventHandler in a Form"
-
 
 
 # A simple WPF form used to call the ExternalEvent
@@ -85,12 +82,11 @@ class game_ModelessForm(WPFWindow):
         self.simple_event_handler = SimpleEventHandler(play)
         self.ext_event = ExternalEvent.Create(self.simple_event_handler)
 
-
     @ERROR_HANDLE.try_catch_error
     def __init__(self):
         self.pre_actions()
 
-        xaml_file_name = "game_UI_ModelessForm.xaml" 
+        xaml_file_name = "game_UI_ModelessForm.xaml"
         WPFWindow.__init__(self, xaml_file_name)
 
         self.title_text.Text = "Monopoly"
@@ -105,16 +101,16 @@ class game_ModelessForm(WPFWindow):
     def init_data_grid(self):
         # this will be replaced by idenpendent dropdown menu Each cell can edit the name, team.
         # the team can be Tean A or B, or indepedent.
-        
+
         from AGENT.PLAYER import Player
         from AGENT.TEAM import Team
-        
+
         names = ["Tom", "Jerry", "Timon", "Pumbaa"]
         teams = [Team(team_name="Solo")] * len(names)
         characters = ["Hat"] * len(names)
-        self.players = [Player(name, team, character) for name, team, character in zip(names, teams, characters)] 
+        self.players = [Player(name, team, character)
+                        for name, team, character in zip(names, teams, characters)]
         self.main_data_grid.ItemsSource = self.players
-        
 
     @ERROR_HANDLE.try_catch_error
     def preview_selection_changed(self, sender, args):
@@ -124,68 +120,50 @@ class game_ModelessForm(WPFWindow):
             self.textblock_display_detail.Text = ""
             return
 
-
     @ERROR_HANDLE.try_catch_error
     def game_start_click(self, sender, args):
         # lock the file, saveas the revit file so not losing original stage.
 
-        
         # validate the player info, make sure all field has valid input
-        
+
         # once started, the data grid is display only, cannot edit again.
 
-
         # set game cycle as 1. Begin game loop.
-        
+
         pass
-    
-    
+
     @ERROR_HANDLE.try_catch_error
     def main_loop(self):
-        
-        
+
         pass
-    
-    
-    
-    
+
         # loop thru all player action. Each call GAME_LOGIC.
-        
+
         # increament the game cycle
-        
+
         # update all UI window display
-        
-        
+
         # check if game is over by call is_game_over
-        
-    
+
     def is_game_over(self):
-        # check if only one players/teams have positive asset. 
+        # check if only one players/teams have positive asset.
         # call finish
-        
+
         # check if manual kill the game
         # call finish
-        
+
         # all seems ok, Then call main_loop again.
-        
-        
+
         pass
-
-
 
     @ERROR_HANDLE.try_catch_error
     def close_Click(self, sender, e):
         # This Raise() method launch a signal to Revit to tell him you want to do something in the API context
         self.Close()
-    
 
     def mouse_down_main_panel(self, sender, args):
-        #print "mouse down"
+        # print "mouse down"
         sender.DragMove()
-
-
-
-
 
 
 ################## main code below #####################
@@ -196,7 +174,4 @@ output.close_others()
 if __name__ == "__main__":
     # Let's launch our beautiful and useful form !
 
-
     modeless_form = game_ModelessForm()
-        
-
